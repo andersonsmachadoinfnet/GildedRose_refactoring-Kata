343891d482d5b0c424f443c78607f814a0745686
unknown <anderson.smachado@al.infnet.edu.br>
2026-02-16 19:05:52 -0300

    Avaliação final de design com as respostas inseridas no como comentário no início da classe GildedRose

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -2,6 +2,22 @@ package com.gildedrose;
 
 import java.util.ArrayList;
 
+/*
+    Respostas para a questão 5 - Avaliação Final de Design.
+    a) A estrutura atual facilita a adição de novos tipos de item? Justifique com base no Princípio Aberto-Fechado.
+       Sim, pois basta criar uma nova classe de item especializada e incluir na lista de updaters que o motor irá
+       funcionar adequadamente. A classe então está aberta para extensão (novos itens), mas fechada para modificação.
+
+    b) A implementação dos ItemUpdater respeita o Princípio da Responsabilidade Única? Explique.
+       Sim, pois cada classe que a implementa faz SOMENTE a atualização dos valores. Não há outra funcionalidade
+       forçada para cada classe. A classe faz uma única coisa.
+
+    c) Alguma violação do Princípio de Substituição de Liskov pode ser identificada em sua hierarquia? Se sim, corrija-a
+       Não vi violação desta regra no código, tendo em vista que a lista de itens é do tipo ItemUpdater (que é uma
+       interface), então as subclasses já estão sendo substituídas pela superclasse (que no caso mais moderno é
+       uma interface)
+ */
+
 class GildedRose {
     Item[] items;
     private ArrayList<ItemUpdater> updaters = new ArrayList<>();
